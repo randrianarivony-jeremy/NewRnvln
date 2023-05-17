5c5734709331414c864753520bdcf9e455239db6
Koloina <koloinaimaginieur@gmail.com>
2023-05-17 21:30:25 +0300

    again

--- src/Component/Article/TextItem.jsx
+++ src/Component/Article/TextItem.jsx
@@ -6,56 +6,55 @@ import { postContext } from "../Post/PostContainer";
 
 const TextItem = () => {
   const { post } = useContext(postContext);
-  const {colorMode}=useColorMode();
+  const { colorMode } = useColorMode();
   const textContainer = useRef();
   const articleSlideRef = useRef();
   const [textOverflow, setTextOverflow] = useState(false);
 
-//   useEffect(() => {
-//     if (textContainer.current.clientHeight < textContainer.current.scrollHeight) {
-//       setTextOverflow(true);
-//     } else setTextOverflow(false);
-//   }, []);
+  useEffect(() => {
+    if (
+      textContainer.current.clientHeight < textContainer.current.scrollHeight
+    ) {
+      setTextOverflow(true);
+    } else setTextOverflow(false);
+  }, []);
 
   return (
     <Swiper
       direction={"vertical"}
       slidesPerView={"auto"}
-      freeMode={{enabled:true,momentum:false}}
-      mousewheel={{enabled:true,forceToAxis:true}}
+      freeMode={{ enabled: true, momentum: false }}
+      mousewheel={true}
       modules={[FreeMode, Mousewheel]}
-      className='article-swiper'
+      className="article-swiper"
     >
-      <SwiperSlide
-        className="article-slide" 
-      ref={articleSlideRef}>
-        <Stack>
-          <Text textAlign="left"
-            ref={textContainer} overflowY='hidden'
-            // height={"calc(100vh - 120px)"}
-          mixBlendMode="hard-light"
-          _after={{
+      <SwiperSlide className="article-slide">
+        <Stack marginX={3}>
+          <Text
+            textAlign="left"
+            ref={textContainer}
+            mixBlendMode="hard-light"
+            _after={{
               position: "absolute",
               bottom: 0,
               left: 0,
-              bg: colorMode==='dark' ? "linear-gradient(transparent 50%,#1a202c 90%)" :
-              "linear-gradient(transparent 50%,white 90%)",
+              bg:colorMode === "dark"
+                  ? "linear-gradient(transparent 50%,#1a202c 90%)"
+                  : "linear-gradient(transparent 50%,white 90%)",
               content: "''",
               width: "100%",
               height: "100%",
               pointerEvents: "none",
-            }
-          }>
+            }}
+          >
             {post.content}
             {post.content}
             {/* {post.content}
             {post.content}
             {post.content}
             {post.content}
-            {post.content}
-            {post.content}
             {post.content} */}
-            </Text>
+          </Text>
         </Stack>
       </SwiperSlide>
     </Swiper>

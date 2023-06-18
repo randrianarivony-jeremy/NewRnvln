d5d587cdec8e438c8138c1b8f17d1fad3957e3c6
Koloina <koloinaimaginieur@gmail.com>
2023-06-18 18:53:13 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import App from "./Controler/App";
 import { apiSlice } from "./Controler/Redux/Features/apiSlice";
 import credentialReducer from "./Controler/Redux/Features/credentialSlice";
 import { theme } from "./Styles/Theme";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 const { ToastContainer } = createStandaloneToast();
 const store = configureStore({
@@ -22,6 +23,10 @@ const store = configureStore({
     getDefaultMiddleware({}).concat([apiSlice.middleware]),
   devTools: process.env.NODE_ENV !== "production",
 });
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
